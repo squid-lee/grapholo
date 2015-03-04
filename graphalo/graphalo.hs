53f03db4bab4edabf4f51365c4a78b3d62f2dc5c
Joseph Slade <slade118118+github@gmail.com>
2015-03-04 16:26:41 +0000

    Added some command line flag parsing

--- graphalo/graphalo.hs
+++ graphalo/graphalo.hs
@@ -4,14 +4,33 @@ import Graphics.Gnuplot.Advanced
 import qualified Graphics.Gnuplot.Terminal.Default as DefaultTerm
 
 import System.Environment (getArgs)
+import Options.Applicative
+import Data.Monoid (mconcat)
+
+data Options = Options {
+  humanReadableFields :: Bool,
+  fields :: [Int]
+}
+  deriving Show
+
 
 main = do
-  args <- getArgs
-  let process = buildProcessor (map read args)
-
+  options <- execParser $ info cli fullDesc
+  let process = buildProcessor (fields options)
   contents <- fmap lines getContents
-
   displayPlot (process contents)
+
+
+cli :: Parser Options
+cli = Options <$>
+      -- switch is boolean argument
+      switch (mconcat [ long "parse-si-suffix-fields"
+                      , short 's'
+                      , help "treat e.g 1.2K as 1200, 7.45M as 7450000 or 13B as 13"
+                      ])  <*>
+      -- (many . (argument str)) gives you many stringy arguments
+      (fmap (map read) $ many $ argument str (metavar "FIELDS"))
+
 
 buildProcessor :: [Int] -> ([String] -> [(Double, Double)])
 buildProcessor [] = zip [1.0..] . map read
